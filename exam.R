c871c239936cb05251168dbc3359d13ce36e80f4
Serge14 <sobukhov@gmail.com>
2017-02-15 00:16:26 +0200

    2nd and improved version

--- exam.R
+++ exam.R
@@ -1,15 +1,16 @@
 setwd("/home/sergiy/Documents/Work/Exam")
-exam = read.csv("text.csv")
+exam = read.csv("questionlist.csv")
 
-# Input file technical information
+# Check number of question groups, 
+# assign zeros to vector that keeps data on number of questions in each group
 NoGroups = length(unique(exam$Group))
 group = rep(0, NoGroups)
 
-# Output
-ticket = ""
+# Output file
+examcards = ""
 
 # Delete output file if it exists
-if (file.exists("ticket")) file.remove("ticket")
+if (file.exists("examcards.txt")) file.remove("examcards.txt")
 
 # Function that read user input, checks if it is integer
 readinteger = function(msg) { 
@@ -25,22 +26,30 @@ NoStudents = readinteger("Enter number students who are expected to take exam: "
 for (i in 1:NoGroups) {
     group[i] = readinteger(paste0("Enter number of questions for group ",
                                   unique(exam$Group)[i],": "))
+    
+    #Check if there are enough questions in the group to prepare examcards
+    while (group[i] > length(exam$Group[exam$Group == unique(exam$Group)[i]])) {
+        print(paste0("Wrong number of questions, you can enter not more than ",
+                     length(exam$Group[exam$Group == unique(exam$Group)[i]])))
+        group[i] = readinteger(paste0("Enter number of questions for group ",
+                                      unique(exam$Group)[i],": "))
+    }
 }
 
 #Step 3: Define questions for each student
 for (i in 1:NoStudents) {
-    ticket = "-----------------------------"
-    ticket=paste0(ticket, "\n", "\n", "Білет №", i, "\n")    # Head of variant
-    write(ticket, "ticket", append = TRUE)
+    examcards = "-----------------------------"
+    examcards=paste0(examcards, "\n", "\n", "Білет №", i, "\n")    # Head of exam card
+    write(examcards, "examcards.txt", append = TRUE)
     
     for (l in 1:NoGroups) {
         k = 1
-        ticket = paste(unique(exam$Group)[l], "\n") # Form group title
-        choice = sample(exam$No[exam$Group==unique(exam$Group)[l]], group[l])                     # Questions for student i
+        examcards = paste(unique(exam$Group)[l], "\n") # Form question group title
+        choice = sample(exam$Questions[exam$Group==unique(exam$Group)[l]], group[l])                     # Questions for student i
     
     for (j in 1:group[l]){
-        ticket = paste0(ticket, k, ". ", exam$Questions[choice[j]], "\n") # Form variant
+        examcards = paste0(examcards, k, ". ", choice[j], "\n") # Form questions for group
         k = k+1
     }
-    write(ticket, "ticket", append = TRUE)      # Write variant into file
+    write(examcards, "examcards.txt", append = TRUE)      # Write group questions into file
 } }
